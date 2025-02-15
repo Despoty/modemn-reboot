3247f3c75a4859b51860374106b9b83bafb8e577
Yuan Tsao <Despoty@github.com>
2025-02-15 16:34:46 +0800

    change driver.py to Chromium core

--- src/driver.py
+++ src/driver.py
@@ -1,24 +1,24 @@
-# 模块功能: 初始化浏览器驱动
-
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service as EdgeService
-from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from log import logger
 from env import CONFIG
 
 def init_driver():
-    """初始化浏览器驱动"""
+    """初始化Chrome浏览器驱动"""
     try:
-        edge_options = EdgeOptions()
-        edge_options.add_argument("--disable-gpu")
-        edge_options.add_argument("--no-sandbox")
-        edge_options.add_argument("--disable-dev-shm-usage")
+        chrome_options = ChromeOptions()
         
         if CONFIG['HEADLESS']:
-            edge_options.add_argument("--headless=new")
+            chrome_options.add_argument("--headless=new")
         
-        service = EdgeService(executable_path=CONFIG['EDGE_DRIVER_PATH'])
-        driver = webdriver.Edge(service=service, options=edge_options)
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920x1080")
+        
+        service = ChromeService(executable_path="/usr/bin/chromedriver")
+        driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.set_page_load_timeout(30)
         return driver
     except Exception as e:
