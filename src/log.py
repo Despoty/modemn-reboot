7289fb1a29b883a7938e111ed7b712328fad3187
Yuan Tsao <Despoty@github.com>
2025-02-15 18:29:15 +0800

    fix bug

--- src/log.py
+++ src/log.py
@@ -2,6 +2,8 @@
 
 import logging
 from datetime import datetime
+import os
+from env import CONFIG
 
 logging.basicConfig(
     level=logging.INFO,
@@ -9,6 +11,10 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
+
+def debug_page(driver, step):
+    logger.info(f"[{step}] 页面标题: {driver.title}")
+    logger.info(f"[{step}] 当前 URL: {driver.current_url}")
 
 if __name__ == "__main__":
     logger()
