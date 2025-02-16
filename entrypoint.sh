76b90874bc821564afb4df84b806360f22621dcb
Yuan Tsao <Despoty@github.com>
2025-02-17 01:36:38 +0800

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-
+# 临时文件用于存放 cron 任务
 CRON_FILE="/tmp/cronjob"
 
 if [ -n "$CRON_SCHEDULE" ]; then
   echo "Using custom cron schedule: $CRON_SCHEDULE"
-  echo "$CRON_SCHEDULE /usr/local/bin/python /app/src/main.py" > "$CRON_FILE"
+  echo "$CRON_SCHEDULE python /app/src/main.py >> /app/logs/main.log 2>&1" > "$CRON_FILE"
 else
   echo "Using default cron schedule: 30 0,12 * * *"
-  echo "30 0,12 * * * /usr/local/bin/python /app/src/main.py" > "$CRON_FILE"
+  echo "30 0,12 * * * python /app/src/main.py >> /app/logs/main.log 2>&1" > "$CRON_FILE"
 fi
 
-# 安装 cron 任务
+# 加载 cron 任务
 crontab "$CRON_FILE"
 
-# 启动 cron 服务（后台运行）
+# 后台启动 cron 服务
 crond -l 8 &
  
-# 防止容器退出，阻塞方式等待
+# 将日志文件的输出实时转发到标准输出
 tail -F /app/logs/main.log
